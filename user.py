89c7c945d1ea5148944a995a16ec6da38ba681ff
xusamson8 <xusamson14@gmail.com>
2025-05-02 00:35:56 -0700

    UC 2

--- user.py
+++ user.py
@@ -43,3 +43,31 @@ class UserManager:
         
         result = self.db.execute_query(query, params)
         return True, "User registered successfully!"
+    
+    def login_user(self, username, password):
+        """UC-2: User login"""
+        query = """
+        MATCH (u:User {screen_name: $username})
+        RETURN u
+        """
+        result = self.db.execute_query(query, {"username": username})
+        
+        if not result:
+            return False, "User not found!"
+            
+        user = result[0]['u']
+        
+        # For testing/demo purposes, if using the Twitter dataset which doesn't have passwords:
+        # Just check if the username exists and log them in
+        if 'password' not in user:
+            self.current_user = user
+            return True, f"Welcome back, {username}!"
+            
+        #Otherwise, verify password with bcrypt
+        stored_password = user['password']
+        if bcrypt.checkpw(password.encode('utf-8'), stored_password.encode('utf-8')):
+            self.current_user = user
+            return True, f"Welcome back, {username}!"
+        else:
+            return False, "Invalid password!"
+            
